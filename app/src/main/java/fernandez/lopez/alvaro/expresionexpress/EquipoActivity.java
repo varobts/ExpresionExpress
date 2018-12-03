e9862e0054ce11891a20e896a8ee4aaf74b637a2
alvaro.lopez.fernandez <alvaro.lopez.fernandez@estudiant.upc.edu>
2018-12-03 13:13:49 +0100

    V0.1.0 - Layout EquipoActivity

--- app/src/main/java/fernandez/lopez/alvaro/expresionexpress/EquipoActivity.java
+++ app/src/main/java/fernandez/lopez/alvaro/expresionexpress/EquipoActivity.java
@@ -1,15 +1,16 @@
 package fernandez.lopez.alvaro.expresionexpress;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class TableroActivity extends AppCompatActivity {
+public class EquipoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tablero);
-
+        setContentView(R.layout.activity_equipo);
     }
 }
