d4d8eff5355fe5e8ab839b14ddb2eb77ab011f65
Alvaro <varobeats@gmail.com>
2019-01-05 15:03:01 +0100

    V0.3.0 - Layout MenuPrincipalActivity

---
+++ app/src/main/java/fernandez/lopez/alvaro/expresionexpress/MenuPrincipalActivity.java
@@ -0,0 +1,25 @@
+package fernandez.lopez.alvaro.expresionexpress;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void OnClickPlay(View view) {
+
+    }
+    public void OnClickMult(View view) {
+
+    }
+    public void OnClickInstru(View view) {
+
+    }
+}
