552519311208ecda050adbd0795da6908ee15bef
Alvaro <varobeats@gmail.com>
2019-01-07 17:21:23 +0100

    V 0.5.0 = Layout MultMenuActivity.

---
+++ app/src/main/java/fernandez/lopez/alvaro/expresionexpress/MultMenuActivity.java
@@ -0,0 +1,21 @@
+package fernandez.lopez.alvaro.expresionexpress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MultMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mult_menu);
+    }
+
+    public void OnClickCreate (View view){
+
+    }
+    public void OnClickJoin (View view){
+
+    }
+}
